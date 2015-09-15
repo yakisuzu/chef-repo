abd047b6229543778370e1f9b57f8357fcd624b1
yakisuzu <yakisuzu@gmail.com>
2015-09-16 00:32:11 +0900

    change path remove setting

--- site-cookbooks/winrc/recipes/path.rb
+++ site-cookbooks/winrc/recipes/path.rb
@@ -1,12 +1,7 @@
-li_env_path = ENV[:PATH].split(';')
-
 node[:winrc][:paths][:machine].each do |path|
   # remove path for suffix of yen
-  path_yen = path + '\\'
-  if li_env_path.include?(path_yen)
-    windows_path path_yen do
-      action :remove
-    end
+  windows_path path + '\\' do
+    action :remove
   end
 
   # append path to machine env
