d19474c70d82809246765995e00eb6383c320283
yakisuzu <yakisuzu@gmail.com>
2015-09-16 22:26:51 +0900

    fix gvim url

--- site-cookbooks/winrc/recipes/gvim.rb
+++ site-cookbooks/winrc/recipes/gvim.rb
@@ -1,6 +1,6 @@
 pd = ENV['ProgramData']
 windows_zipfile pd do
-  source 'http//files.kaoriya.net/goto/vim74w32'
+  source 'http://files.kaoriya.net/goto/vim74w32'
   action :unzip
   not_if {::File.exists?(pd + '\\vim74-kaoriya-win32')}
 end
