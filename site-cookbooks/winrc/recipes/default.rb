4fa821d8727936331b2f8b1b062c031d21b96c7a
yakisuzu <yakisuzu@gmail.com>
2016-02-01 00:24:12 +0900

    pacman setting

--- site-cookbooks/winrc/recipes/default.rb
+++ site-cookbooks/winrc/recipes/default.rb
@@ -1,7 +1,15 @@
-include_recipe 'winrc::choco_package'
+# path manegement
 include_recipe 'winrc::path'
+
+# install application
+include_recipe 'winrc::choco_package'
 include_recipe 'winrc::msys2'
 include_recipe 'winrc::gvim'
 include_recipe 'winrc::dotfiles'
 include_recipe 'winrc::stickies'
+
+# pacman
+include_recipe 'winrc::pacman'
+
+# init repos
 include_recipe 'winrc::repository_init'
---
+++ site-cookbooks/winrc/recipes/pacman.rb
@@ -0,0 +1,22 @@
+# https://ja.wikipedia.org/wiki/Pacman
+def install_pacman(st_name, st_package)
+  unless `where #{st_package}`.match(/#{st_package}/) then
+    return batch st_name do
+      code "pacman -S #{st_package} --noconfirm"
+      action :run
+    end
+  else
+    return log "#{st_package} installed" do
+      action :nothing
+    end
+  end
+end
+
+batch 'update' do
+  code "pacman -Syu --noconfirm"
+  action :run
+end
+
+install_pacman('install python3', 'python')
+install_pacman('install python2', 'python2')
+install_pacman('install tig', 'tig')
